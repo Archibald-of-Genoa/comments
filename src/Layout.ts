7d199a614c313eb4e4260d4111f23ba563b3a969
Archie <archibaldofgenoa@yandex.ru>
2024-05-10 16:30:25 +0300

    simple version of Comment block

--- src/Layout.ts
+++ src/Layout.ts
@@ -1,6 +1,7 @@
 import arrowImage from "@/assets/Arrow.svg";
 import heartImage from "@/assets/heart.svg";
 import replyImage from "@/assets/reply.svg";
+import { wrapper } from ".";
 
 class Layout {
   private mockCount: number;
@@ -13,7 +14,6 @@ class Layout {
   private init() {
     this.addContentMock(this.mockCount);
     this.commentsFilter();
-    this.newCommentForm();
   }
 
   private addContentMock(mockCount: number | null): void {
@@ -41,7 +41,7 @@ class Layout {
         arrow.classList.toggle("select-rotated");
       };
 
-      select.addEventListener("click", toggleArrowRotation);
+      select.addEventListener("mousedown", toggleArrowRotation);
       select.addEventListener("mouseup", toggleArrowRotation);
     }
     if (heart) {
@@ -52,67 +52,6 @@ class Layout {
     }
   }
 
-  newCommentForm(): HTMLDivElement | null {
-    const wrapper: HTMLDivElement | null = document.querySelector(".wrapper");
-    const warning: HTMLDivElement | null = document.querySelector(".warning");
-    const sendBtn: HTMLButtonElement | null =
-      document.querySelector(".send-btn");
-    const commentForm = document.getElementById(
-      "commentForm"
-    ) as HTMLFormElement | null;
-    const textarea = document.getElementById(
-      "comment"
-    ) as HTMLTextAreaElement | null;
-
-    const commentsBlock: HTMLDivElement = document.createElement("div");
-    commentsBlock.classList.add("commentsBlock");
-    commentsBlock.innerHTML = /*html*/ `
-
-    <div class="newCommentBlock">
-      <img class="userAvatar" alt="Аватар пользователя" />
-      <div div class="userNameAndFormWrapper">
-        <div class="userNameWrapper">
-          <span class="userName"></span>
-
-          <div class="warning">Макс. 1000 символов</div>
-        </div>
-
-        <form id="commentForm" action="submit">
-          <label for="comment"></label>
-          <textarea
-            name="comment"
-            id="comment"
-            cols="30"
-            rows="10"
-            maxlength="1000"
-            placeholder="Введите текст сообщения"
-            required
-          ></textarea>
-
-          <button type="submit" class="send-btn btn">Отправить</button>
-        </form>
-      </div>
-    </div>
-  
-    `;
-    wrapper.appendChild(commentsBlock);
-
-    // textarea.addEventListener("input", function () {
-    //   const messageLength = textarea.value.length;
-
-    //   if (messageLength >= 10) {
-    //     sendBtn.disabled = true;
-    //     warning.style.display = "block";
-    //   } else {
-    //     warning.style.display = "none";
-    //     sendBtn.disabled = false;
-    //   }
-    // });
-
-    
-
-    return commentsBlock;
-  }
 }
 
 export default Layout;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,19 @@ import Layout from "./Layout";
 import UIManager from "./UIManager";
 import LoadUserData from "./LoadUserData";
 
+export const wrapper: HTMLDivElement | null =
+  document.querySelector(".wrapper");
+export const warning: HTMLDivElement | null =
+  document.querySelector(".warning");
+export const sendBtn: HTMLButtonElement | null =
+  document.querySelector(".send-btn");
+export const commentForm = document.getElementById(
+  "commentForm"
+) as HTMLFormElement | null;
+export const textarea = document.getElementById(
+  "comment"
+) as HTMLTextAreaElement | null;
+
 new Layout(9);
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -20,8 +33,18 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error("Не удалось загрузить данные пользователя");
     }
   });
-});
 
-document.addEventListener("submit", function (event: Event) {
-  event.preventDefault();
+    textarea.addEventListener("input", function () {
+      console.log(textarea.value);
+      //   const messageLength = textarea.value.length;
+      //   sendBtn.disabled = true;
+      //   warning.style.display = "block";
+      //   if (messageLength >= 10) {
+      //     sendBtn.disabled = true;
+      //     warning.style.display = "block";
+      //   } else {
+      //     warning.style.display = "none";
+      //     sendBtn.disabled = false;
+      //   }
+    });
 });
